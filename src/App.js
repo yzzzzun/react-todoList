32ba0a8a332c5c0d6bb13f88d5dd65b7b34fe1a7
youngjun.jin <yzzzzun@gmail.com>
2020-01-05 20:55:34 +0900

    [TDA-11] App component todos state 사용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'react.js 학습', checked: true },
+    { id: 2, text: 'node.js 학습', checked: true },
+    { id: 3, text: 'mongoDB 학습', checked: false },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
