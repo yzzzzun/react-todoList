a4f9b92b2acd1eb06e8c70a23623e4bfcd3d7c8b
youngjun.jin <yzzzzun@gmail.com>
2020-01-14 22:34:37 +0900

    [TDA-14] useReducer 사용해 최적화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useRef, useReducer, useCallback } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
@@ -16,8 +16,24 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(2501);
 
@@ -27,20 +43,23 @@ const App = () => {
       text: text,
       checked: false,
     };
-    setTodos(todos => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
+    // setTodos(todos => todos.concat(todo));
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback(id => {
-    setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
+    // setTodos(todos => todos.filter(todo => todo.id !== id));
   }, []);
 
   const onToggle = useCallback(id => {
-    setTodos(todos =>
-      todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
-    );
+    dispatch({ type: 'TOGGLE', id });
+    // setTodos(todos =>
+    //   todos.map(todo =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    //   ),
+    // );
   }, []);
 
   return (
