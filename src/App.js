6ef8e5e04878247b7284accafc8ee26b8af335f4
youngjun.jin <yzzzzun@gmail.com>
2020-01-14 21:10:39 +0900

    [TDA-14] 2500개의 todo data 추가

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'react.js 학습', checked: true },
-    { id: 2, text: 'node.js 학습', checked: true },
-    { id: 3, text: 'mongoDB 학습', checked: false },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
 
